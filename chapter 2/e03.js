bd4589b1324c78691629a50e8f089bde34e38864
Ian Andersen <ianderse@mac.com>
2014-12-14 13:23:21 -0700

    finish e03

--- chapter 2/e03.js
+++ chapter 2/e03.js
@@ -1,9 +1,22 @@
-for(var i = 0; i <= 8; i += 1)
-	for(var j = 0; j <= 8; j += 1)
-		if (j == 8)
-			console.log(' ');
-		else if (j % 2 == 0 && i % 2 == 0)
-			process.stdout.write(" ");
-		else
-			process.stdout.write("#");
+var grid = '';
 
+for(var i = 0; i < 8; i += 1) {
+	for(var j = 0; j < 8; j += 1) {
+    if (j % 2 == 0) {
+      if (i % 2 == 0) {
+        grid += ' ';
+      } else {
+        grid += '#';
+      }
+    } else {
+      if (i % 2 == 0) {
+        grid += '#';
+      } else {
+        grid += ' ';
+      }
+    }
+  };
+  grid += "\n";
+};
+
+console.log(grid);
